b237470c8da6abda5b8b95e6e059bcebe078bf40
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-01-03 02:45:16 +0530

    Add files via upload

---
+++ array/mergeOverLappingInterval.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+bool comp(Interval i1, Interval i2)
+{
+    return (i1.start < i2.start);
+}
+ 
+vector<Interval> Solution::merge(vector<Interval> &A) 
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    vector<Interval> v;
+ 
+    sort(A.begin(), A.end(), comp);
+ 
+    int len = A.size();
+ 
+    for(int i=0;i<len;i++)
+    {
+        Interval temp;
+        temp.start = A[i].start;
+        temp.end = A[i].end;
+ 
+        int j;
+ 
+        for(j=i+1;j<len;j++)
+        {
+            if(A[j].start > temp.end)
+                break;
+ 
+            temp.end = max(temp.end, A[j].end);
+        }
+ 
+        i = j-1;
+ 
+        v.push_back(temp);
+    }
+ 
+    return v;
+}
